483ad99f05ac2c0bbfd8c934fdf572e97f2a6660
EnglerJan <hockye@seznam.cz>
2025-11-18 01:22:14 +0100

    add pyright lsp

--- init.lua
+++ init.lua
@@ -256,6 +256,17 @@ require("lazy").setup({
 	},
 
 	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*", -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
+	},
+
+	{
 		"lervag/vimtex",
 		lazy = false, -- Load immediately
 		config = function()
@@ -417,15 +428,19 @@ require("lazy").setup({
 
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			local prefix = "<leader>f"
+			vim.keymap.set("n", prefix .. "h", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", prefix .. "k", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", prefix .. "f", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", prefix .. "c", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[F]ind [C]onfig" })
+			vim.keymap.set("n", prefix .. "s", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", prefix .. "w", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", prefix .. "g", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", prefix .. "d", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", prefix .. "r", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", prefix .. ".", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 			-- Slightly advanced example of overriding default behavior and theme
@@ -680,7 +695,7 @@ require("lazy").setup({
 			local servers = {
 				-- clangd = {},
 				-- gopls = {},
-				-- pyright = {},
+				pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
